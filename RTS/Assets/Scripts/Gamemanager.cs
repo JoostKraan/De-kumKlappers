8dd693f91cd56094fe37086e917bd8b79c378079
MichelBoerema <oome.gerrie@gmail.com>
2023-09-15 15:13:43 +0200

    ResourceCollecting and Gamemanager

---
+++ RTS/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public int money = 0;
+    public int wood = 0;
+    public int stone = 0;
+    public int iron = 0;
+}
