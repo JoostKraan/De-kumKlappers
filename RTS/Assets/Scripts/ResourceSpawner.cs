4bfe9553cf142b540ed67b64a009d12486db26dd
MichelBoerema <oome.gerrie@gmail.com>
2023-11-02 11:16:19 +0100

    tags + model

--- RTS/Assets/Scripts/ResourceSpawner.cs
+++ RTS/Assets/Scripts/ResourceSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ResourceSpawner : MonoBehaviour
 {
     [Header("Spawn Settings")]
-    public GameObject resourcePrefab;
+    public GameObject[] resourcePrefabs; // Array of different resource prefabs
     public float spawnChance;
 
     [Header("Raycast Settings")]
@@ -18,6 +18,7 @@ public class ResourceSpawner : MonoBehaviour
     {
         SpawnResources();
     }
+
     void SpawnResources()
     {
         for (float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenChecks)
@@ -25,11 +26,14 @@ public class ResourceSpawner : MonoBehaviour
             for (float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenChecks)
             {
                 RaycastHit hit;
-                if(Physics.Raycast(new Vector3(x, heightCheck, z ),Vector3.down, out hit, rangecheck, layerMask))
+                if (Physics.Raycast(new Vector3(x, heightCheck, z), Vector3.down, out hit, rangecheck, layerMask))
                 {
-                    if(spawnChance > Random.Range(0, 101))
+                    if (spawnChance > Random.Range(0, 101))
                     {
-                        Instantiate(resourcePrefab, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)),transform);
+                        // Randomly select a prefab from the array
+                        GameObject randomPrefab = resourcePrefabs[Random.Range(0, resourcePrefabs.Length)];
+
+                        Instantiate(randomPrefab, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), transform);
                     }
                 }
             }
