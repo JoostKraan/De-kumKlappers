611c6b0f3cc8809063476466b5f148eacef592b3
MichelBoerema <oome.gerrie@gmail.com>
2023-11-30 18:26:42 +0100

    working UI

--- RTS/Assets/Scripts/UI/Buttons.cs
+++ RTS/Assets/Scripts/UI/Buttons.cs
@@ -1,37 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
-
-
-    [SerializeField] GameObject shop;
+    private Gamemanager gamemanager;
+    [SerializeField] private GameObject pauseScreen;
 
     [Header("boloean")]
-    bool shopIsOpen = false;
-    public void OpenShop()
+    bool pauseScreenIsActive = false;
+    [Header("Text")]
+    public Text wood;
+    public Text stone;
+    public Text iron;
+    private void Start()
     {
-        if (shopIsOpen)
-        {
-            shop.SetActive(false);
-            shopIsOpen = false;
-        }
-        else if (shopIsOpen == false)
-        {
-            shop.SetActive(true);
-            shopIsOpen = true;
-        }
-
+        gamemanager = GameObject.FindObjectOfType<Gamemanager>();
     }
     private void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        wood.text = gamemanager.wood.ToString();
+        stone.text = gamemanager.stone.ToString();
+        iron.text = gamemanager.iron.ToString();
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if (shopIsOpen)
+            if(pauseScreenIsActive == false)
             {
-                shop.SetActive(false);
+                pauseScreen.SetActive(true);
+                pauseScreenIsActive = true;
             }
+            else if(pauseScreenIsActive)
+            {
+                pauseScreen.SetActive(false);
+                pauseScreenIsActive = false;
+            }            
         }
+        if(pauseScreenIsActive)
+        {
+            Time.timeScale = 0f;
+        }
+        if(pauseScreenIsActive == false)
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+    public void PauseGame()
+    {
+        pauseScreenIsActive = true;
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        pauseScreenIsActive = false;
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    public void OpenOptionsMenu()
+    {
+        
+    }
+    public void ExitGame()
+    {
+
     }
 }
