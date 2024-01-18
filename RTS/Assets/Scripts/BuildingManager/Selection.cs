2409f41545f7181cdccb44ce70e07609ce1e563c
Tempy <113506941+UsedTempy@users.noreply.github.com>
2024-01-18 11:32:11 +0100

    Fixed Placement

--- RTS/Assets/Scripts/BuildingManager/Selection.cs
+++ RTS/Assets/Scripts/BuildingManager/Selection.cs
@@ -19,25 +19,21 @@ public class Selection : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-       if (Input.GetMouseButtonDown(0))
-       {
+    void Update() {
+       if (Input.GetMouseButtonDown(0)) {
            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
            RaycastHit hit;
-           if (Physics.Raycast(ray, out hit, 1000))
-           {
-                if (hit.collider.gameObject.CompareTag("PlaceObject"))
-                {
+           if (Physics.Raycast(ray, out hit, 1000)) {
+                if (hit.collider.gameObject.CompareTag("PlaceObject")) {
                     Select(hit.collider.gameObject);
                 }
                 else Deselect();
             }
        }
-       if (Input.GetMouseButtonDown(1))
-        {
+
+       if (Input.GetMouseButtonDown(1)) {
             Deselect();
-        }
+       }
     }
 
     private void Select(GameObject obj)
