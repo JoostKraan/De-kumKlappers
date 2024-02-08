8f782e4264ed697f702d0492fd65a3512b718efd
Sjoerd <113519405+DreojsNL@users.noreply.github.com>
2024-02-08 22:09:56 +0100

    Unit Attacked

--- RTS/Assets/Scripts/WorkerScripts/New Units/UnitMovement.cs
+++ RTS/Assets/Scripts/WorkerScripts/New Units/UnitMovement.cs
@@ -19,7 +19,7 @@ public class UnitMovement : MonoBehaviour
     // Flag to indicate if the building is being attacked
     public bool isAttackingBuilding = false;
 
-    public Animator animator;
+    //public Animator animator;
     void Start()
     {
         myCam = Camera.main;
@@ -34,9 +34,9 @@ public class UnitMovement : MonoBehaviour
         FindNearestEnemyBuilding();
         if (Input.GetMouseButtonDown(1))
         {
-            animator.SetBool("Idle", false);
-            animator.SetBool("Fighting", false);
-            animator.SetBool("Walking", true);
+          //  animator.SetBool("Idle", false);
+          //  animator.SetBool("Fighting", false);
+          //  animator.SetBool("Walking", true);
             RaycastHit hit;
             Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
 
@@ -53,9 +53,9 @@ public class UnitMovement : MonoBehaviour
         // Attack enemy building if it's time and within range
         if (nearestBuilding != null && nearestDistance <= attackRange && timeSinceLastAttack >= attackInterval)
         {
-            animator.SetBool("Idle", false);
-            animator.SetBool("Walking", false);
-            animator.SetBool("Fighting", true);
+         //   animator.SetBool("Idle", false);
+         //  animator.SetBool("Walking", false);
+         //   animator.SetBool("Fighting", true);
             AttackEnemyBuilding();
             timeSinceLastAttack = 0f; // Reset the timer
         }
