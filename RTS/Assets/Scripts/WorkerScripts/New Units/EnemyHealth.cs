c0ec990ff47171648b4b48b77e4c261809be213c
Joost <113507203+JoostKraan@users.noreply.github.com>
2024-02-08 11:46:02 +0100

    temp changes

---
+++ RTS/Assets/Scripts/WorkerScripts/New Units/EnemyHealth.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public bool IsDead { get { return currentHealth <= 0; } }
+
+    // Events for when the object takes damage or dies
+    public delegate void OnTakeDamageDelegate(int damage);
+    public event OnTakeDamageDelegate OnTakeDamage;
+
+    public delegate void OnDeathDelegate();
+    public event OnDeathDelegate OnDeath;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (IsDead)
+            return;
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+
+        OnTakeDamage?.Invoke(damage);
+    }
+
+    void Die()
+    {
+        OnDeath?.Invoke();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public void SetMaxHealth(int value)
+    {
+        maxHealth = value;
+        currentHealth = maxHealth;
+    }
+}
--- RTS/Assets/Scripts/WorkerScripts/New Units/UnitMovement.cs
+++ RTS/Assets/Scripts/WorkerScripts/New Units/UnitMovement.cs
@@ -9,15 +9,16 @@ public class UnitMovement : MonoBehaviour
     public LayerMask ground;
     public LayerMask enemyBuilding;
 
-    // Store the RaycastHit for enemy building here
+    public bool isEnemy;
+   
     RaycastHit enemyHit;
 
-    // Damage per tick and interval between ticks
+  
     public int damagePerTick = 10;
     public float tickInterval = 5f;
 
-    // Flag to indicate if the building is being attacked
-    bool isAttackingBuilding = false;
+    
+    public bool isAttackingBuilding = false;
 
     void Start()
     {
@@ -38,7 +39,7 @@ public class UnitMovement : MonoBehaviour
             }
             if (!isAttackingBuilding && Physics.Raycast(ray, out hit, Mathf.Infinity, enemyBuilding))
             {
-                // Store the RaycastHit for enemy building
+               
                 enemyHit = hit;
                 StartCoroutine(AttackEnemyBuilding());
             }
@@ -47,45 +48,42 @@ public class UnitMovement : MonoBehaviour
 
     IEnumerator AttackEnemyBuilding()
     {
-        isAttackingBuilding = true; // Set flag to true to prevent further attacks
+        isAttackingBuilding = true; 
 
-        // Pause the NavMeshAgent's movement
+    
         myAgent.isStopped = true;
 
-        // Set destination to the enemy building position
+     
         myAgent.SetDestination(enemyHit.point);
 
         Debug.Log("Attacking enemy building!");
 
-        // Print the name of the object hit by the raycast
+       
         Debug.Log("Object hit: " + enemyHit.collider.gameObject.name);
 
-        // Assuming the enemy building has a script with a health value
-        // You need to access that script to decrease its health
-        // For example, if the script is called EnemyBuildingHealth, and it has a public method TakeDamage(int amount)
-        // You can do something like this:
+       
         EnemyBuildingHealth enemyHealth = enemyHit.collider.GetComponent<EnemyBuildingHealth>();
         if (enemyHealth != null)
         {
-            // Continue dealing damage until the enemy building's health reaches zero
+           
             while (enemyHealth.health > 0)
             {
-                // Deal damage per tick
+                
                 enemyHealth.TakeDamage(damagePerTick);
 
                 Debug.Log("Dealing damage to enemy building!");
 
-                // Wait for the tick interval before dealing damage again
+                
                 yield return new WaitForSeconds(tickInterval);
             }
 
-            // Enemy building destroyed or health reached zero
+           
             Debug.Log("Enemy building destroyed!");
         }
 
-        // Resume the NavMeshAgent's movement
+   
         myAgent.isStopped = false;
 
-        isAttackingBuilding = false; // Reset flag after the building is destroyed
+        isAttackingBuilding = false; 
     }
 }
